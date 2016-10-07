a258e75bc619e4186847e57efafaf50f47c5aa38
Gnimuc Key <GnimucKey@gmail.com>
2016-10-07 22:50:09 +0800

    Misc. fix

--- 3-4-Texture Synthesis and Analysis/Gibbs-julia/Gibbs.jl
+++ 3-4-Texture Synthesis and Analysis/Gibbs-julia/Gibbs.jl
@@ -4,7 +4,7 @@ using Distributions, Images
 
 # Sampling Algorithm 2
 # Figure 3.8: Generating a texture using a Gibbs sampler.
-function gibbs!{T<:Real}(L::Vector{T}, F::Matrix{T}, β::Vector{T}, α::T=0)
+function gibbs!(L::Vector, F::Matrix, β::Vector, α=0)
     # S = {(i,j) | 1 ≤ i,j ≤ n} correspond to the pixels of an n×n image in the 2D plane
     S = CartesianRange(size(F))
     S₁, Sn = first(S), last(S)
@@ -15,11 +15,11 @@ function gibbs!{T<:Real}(L::Vector{T}, F::Matrix{T}, β::Vector{T}, α::T=0)
         #   - o -  =>   o   o -    o    o
         #   ╱ | ╲       β1   β2   β3   β4   <= interaction parameters
         N = CartesianRange(max(S₁, i-S₁), min(Sn, i+S₁))
-        U = Vector{L}(0)
+        U = Float64[]
         # (2.1) compute pl = P{fᵢ = l | fNᵢ} for all l ∈ L
-        for l in L
+        for l ∈ L
             ΣV₂ = 0
-            for í ∈ N
+            for i′ ∈ N
                 if i′ != i    # exclude o o
                     #  V₂(f) = {  β  if sites on clique {i,i′} = c ∈ C₂ have the same label
                     #          { -β  otherwise
@@ -40,7 +40,7 @@ function gibbs!{T<:Real}(L::Vector{T}, F::Matrix{T}, β::Vector{T}, α::T=0)
             # U(fᵢ) = V₁(fᵢ) + Σ V₂(fᵢ)
             #       =  αfᵢ   + Σ βᵢᵢ′fᵢfᵢ′
             # when fᵢ = l (see (2.1) above):
-            Ul = αl + ΣV₂
+            Ul = α*l + ΣV₂
             push!(U, Ul)
         end
         # P(fᵢ|fn) = e^U(fᵢ) / Σᵢe^U(i)
@@ -50,7 +50,7 @@ function gibbs!{T<:Real}(L::Vector{T}, F::Matrix{T}, β::Vector{T}, α::T=0)
         # sample from multinomial distribution
         Pl = rand(Multinomial(1, P), 1)
         # (2.2) set fᵢ to l with probability Pl
-        f[i] = find(Pl)[]
+        F[i] = L[findfirst(Pl)]
     end
 end
 
